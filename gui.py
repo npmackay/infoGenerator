e665128fd9d0dfc41318e945d882dc5849952d6b
Noah <noahpmackay@gmail.com>
2023-01-20 12:13:57 -0500

    update

--- gui.py
+++ gui.py
@@ -11,18 +11,17 @@ window = Tk()
 window.title("INFO GENERATOR")
 window.geometry(windowDim)
 window.resizable(False, False)
-
 # CREATING USER CONTROLS
 # each button will print a list of items (number of items will be come from the entry userValue)
 userEntry = Entry(window)
+#userValue = int(userValue)
 
+userValue = 3
 
 userEntry.place(x=450, y=200)
-userEntry.insert(0, 2)
+userEntry.insert(0, userValue)
 updateButton = Button(window, text="UPDATE",
                       ).place(x=475, y=225)
-
-outputValue = int(userEntry.get())
 
 userValueLabel = Label(
     window, text="ENTER AMOUNT OF RECORDS YOU WOULD LIKE").place(x=400, y=170)
@@ -30,45 +29,49 @@ label = Label(window, text="INFOMATION GENERATOR by Noah Mackay ",
               background='#9999ff').pack()
 
 
-def outputEmails():
+def outputEmails(userValue):
+    # function receives the amount of records the user wants to print
     # function will be called when email button is clicked
     # this function will clear the output file and then read open it
     # will call the generateEmail function from scripts.py file
     # outputs the amount of records based on the user input in the entry text box
-
-    outputFile = scripts.generateEmail(outputValue)
+    outputFile = scripts.generateEmail(userValue)
     file = open('output.txt', 'w').close()
     file = open('output.txt', 'w')
     file.write(str(outputFile))
 
 
-def outputNames():
-
-    outputFile = scripts.generateName(outputValue)
+def outputNames(userValue):
+    # function receives the amount of records the user wants to print
+    # function will be called when email button is clicked
+    # this function will clear the output file and then read open it
+    # will call the generateEmail function from scripts.py file
+    # outputs the amount of records based on the user input in the entry text box
+    outputFile = scripts.generateName(userValue)
     file = open('output.txt', 'w').close()
     file = open('output.txt', 'w')
     file.write(str(outputFile))
 
 
-def outputCost():
+def outputCost(userValue):
 
-    outputFile = scripts.generateCost(outputValue)
+    outputFile = scripts.generateCost(userValue)
     file = open('output.txt', 'w').close()
     file = open('output.txt', 'w')
     file.write(str(outputFile))
 
 
-def outputProduct():
+def outputProduct(userValue):
 
-    outputFile = scripts.generateProduct(outputValue)
+    outputFile = scripts.generateProduct(userValue)
     file = open('output.txt', 'w').close()
     file = open('output.txt', 'w')
     file.write(str(outputFile))
 
 
-def outputPhoneNumber():
+def outputPhoneNumber(userValue):
 
-    outputFile = scripts.generatePhoneNumber(outputValue)
+    outputFile = scripts.generatePhoneNumber(userValue)
     file = open('output.txt', 'w').close()
     file = open('output.txt', 'w')
     file.write(str(outputFile))
@@ -77,15 +80,16 @@ def outputPhoneNumber():
 
 
 emailButton = Button(window, text="EMAILS",
-                     command=outputEmails()).place(x=40, y=50)
+                     command=outputEmails(userValue)).place(x=40, y=50)
 nameButton = Button(
-    window, text="FIRST AND LAST NAMES", command=outputNames()).place(x=40, y=100)
-costButton = Button(window, text="PRICES",
-                    command=outputCost()).place(x=40, y=150)
+    window, text="FIRST AND LAST NAMES", command=outputNames(userValue)).place(x=40, y=100)
+
 productButton = Button(window, text="PRODUCTS",
-                       command=outputProduct()).place(x=40, y=200)
+                       command=outputProduct(userValue)).place(x=40, y=200)
 phoneNumberButton = Button(
     window, text="PHONE NUMBERS").place(x=40, y=250)
+costButton = Button(window, text="PRICES",
+                    command=outputCost(userValue)).place(x=40, y=150)
 
 
 window.mainloop()
